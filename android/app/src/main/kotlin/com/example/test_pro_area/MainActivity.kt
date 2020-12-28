be20bea5c9a515d72838423ee493d380c7d13543
Okladnoj <dmytrijokladnoj@gmail.com>
2020-12-28 09:57:14 +0200

    Fix Splash Screen

--- android/app/src/main/kotlin/com/example/test_pro_area/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test_pro_area/MainActivity.kt
@@ -5,5 +5,20 @@ import io.flutter.embedding.android.FlutterActivity
 import android.os.Build
 import android.view.ViewTreeObserver
 import android.view.WindowManager
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    
+    // override fun provideSplashScreen():SplashScreen {
+    //     return MyFlutterFragment()
+    // }
 }
+
+// class MyFlutterFragment:FlutterFragment() {
+  
+//   protected fun provideSplashScreen():SplashScreen {
+//     // Load the splash Drawable.
+//     val splash = getResources().getDrawable(R.drawable.launch_background)
+//     // Construct a DrawableSplashScreen with the loaded splash
+//     // Drawable and return it.
+//     return DrawableSplashScreen(splash)
+//   }
+// }
